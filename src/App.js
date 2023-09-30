b75110d7f0e8806035117f7f9e35cedccb4c73f7
Nilesh <nileshshindes98@gmail.com>
2023-10-01 01:49:03 +0530

    [Nilesh] : Set Route path for DashBoard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SignIn from './Pages/signIn/SignIn';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './Pages/signUp/SignUp';
+import DashBoard from './Pages/dashboard/DashBoard';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path='/' element={<SignIn/>}/>
        <Route path='/signup' element={<SignUp />}/>
+       <Route path='/dashboard' element={<DashBoard />}/>
+
       </Routes>
     </Router>
 
