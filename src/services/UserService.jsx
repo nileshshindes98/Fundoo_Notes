a91db6259f7fa75f03cf959d5f2fea0d6e5bad8f
Nilesh <nileshshindes98@gmail.com>
2023-09-30 18:57:00 +0530

    [Nilesh] : create function for sign up the new user with help POST API

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -11,3 +11,13 @@ export let signIn = async (data) => {
     localStorage.setItem("token", token);
     return response;
 };
+export let signUp = async (data) => {
+    let response = await axios.post(
+      "http://fundoonotes.incubation.bridgelabz.com/api/user/userSignUp",
+      data
+    );
+    console.log(data);
+    console.log(response);
+    return response;
+  };
+  
