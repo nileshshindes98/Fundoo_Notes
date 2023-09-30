5fd205ea1d8edcfb5acad92a9988944d2bb8decc
Nilesh <nileshshindes98@gmail.com>
2023-09-30 18:58:46 +0530

    [Nilesh] : Add Route path for navigate the SignUp page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import SignIn from './Pages/signIn/SignIn';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import SignUp from './Pages/signUp/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<SignIn/>} />
+        <Route path='/' element={<SignIn/>}/>
+       <Route path='/signup' element={<SignUp />}/>
       </Routes>
     </Router>
 
