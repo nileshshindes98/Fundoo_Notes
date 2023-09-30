8b676a4e702565e2b23ef46f2f00a1ce4596990c
Nilesh <nileshshindes98@gmail.com>
2023-09-30 17:29:38 +0530

    [Nilesh] : set path for Sign In

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import SignIn from './pages/signIn';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<SignIn/>} />
+      </Routes>
+    </Router>
+
   );
 }
 
