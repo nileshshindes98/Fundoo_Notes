85c798f47927b6aab2be43921d225d103b08624d
Nilesh <nileshshindes98@gmail.com>
2023-10-01 19:39:07 +0530

    [Nilesh] : Create List view of Notes

---
+++ src/component/note/displayNote/ListNote.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+import IconButton from "@mui/material/IconButton";
+import { Typography, Box, Grid, Paper } from "@mui/material/";
+import RemindMe from "../../cardComponent/RemindMe";
+import Collaborate from "../../cardComponent/Collaborate";
+import ColorPallete from "../../cardComponent/ColorPallete";
+import InsertPhotoOutlinedIcon from "@mui/icons-material/InsertPhotoOutlined";
+import Archive from "../../cardComponent/Archive";
+
+import Delete from "../../cardComponent/Delete";
+import Pin from "../../cardComponent/Pin";
+import { Container } from "@mui/system";
+
+const ListNote = ( {title,description}) => {
+  return (
+    
+      <Container maxWidth="sm">
+        <Box sx={{ width:"130%" , m:1 }}>
+          <Paper style={{ border: "1px solid grey", padding: "8px" }}>
+            <Grid sx={{  }}>
+              <Grid item>
+                <Typography>{title}</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>{description}</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>
+                  <IconButton aria-label="Remainder">
+                    <RemindMe />
+                  </IconButton>
+
+                  <IconButton>
+                    <ColorPallete />
+                  </IconButton>
+
+                  <IconButton>
+                    <Collaborate />
+                  </IconButton>
+
+                  <IconButton>
+                    <InsertPhotoOutlinedIcon />
+                  </IconButton>
+
+                  <IconButton>
+                    <Archive />
+                  </IconButton>
+
+                  <IconButton>
+                    <Delete />
+                  </IconButton>
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Box>
+      </Container>
+
+  );
+};
+
+export default ListNote;
