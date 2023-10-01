7616d8614822051deb3de22d1876da696b19d95f
Nilesh <nileshshindes98@gmail.com>
2023-10-01 19:40:27 +0530

    [Nilesh] : Add API for Save the note and render it

---
+++ src/services/DataService.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "http://fundoonotes.incubation.bridgelabz.com/api/notes";
+
+const headerConfig = () => {
+  return {
+    headers: {
+      Authorization: localStorage.getItem("token"),
+      "Content-Type": "application/json",
+    },
+  };
+};
+
+export const addNote = async (noteData) => {
+  const response = await axios.post(
+    `${BASE_URL}/addNotes`,
+    noteData,
+    headerConfig()
+  );
+  return response;
+};
+export const getNotes = async () => {
+    const response = await axios.get(`${BASE_URL}/getNotesList`, headerConfig());
+    return response;
+  
+  };
