e371427f30132870115287cefe608316c771037a
Nilesh <nileshshindes98@gmail.com>
2023-10-01 19:31:25 +0530

    [Nilesh] : Added toggle functionality to display note in grid and list view

--- src/component/appBar/Toggle.jsx
+++ src/component/appBar/Toggle.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ViewStreamRoundedIcon from '@mui/icons-material/ViewStreamRounded';
 import { IconButton } from '@mui/material';
 
-const Toggle = () => {
+const Toggle = ({toggleDisplay}) => {
   return (
     <div>
-      <IconButton  size="large" color="black">
+      <IconButton  size="large" color="black"onClick={toggleDisplay}>
         <ViewStreamRoundedIcon />
       </IconButton>
     </div>
